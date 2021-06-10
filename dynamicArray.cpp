303eb9f81bdefb3c2712a495109716147db73e2e
Kunal-Sharma213 <68760164+Kunal-Sharma213@users.noreply.github.com>
2021-06-10 19:44:56 +0530

    Update dynamicArray.cpp
    A separate function made to do the task. Also instead of making an array of size M, a vector of size M is now created.

--- dynamicArray.cpp
+++ dynamicArray.cpp
@@ -1,105 +1,104 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-template <class T>
-class Vector
+// template <class T> // creating template to handle various types of input
+// class Vector
+// {
+// public:
+//     T *arr;
+//     Vector(int M) // if a floating value is entered as the M, it will comverted to int
+//     {
+//         arr = new T[M];
+//     }
+// };
+void create_vector(string type, int M)
 {
-public:
-    T *arr;
-    Vector(int size) // if a floating value is entered as the size, it will comverted to int
+    if (type.compare("int") == 0)
     {
-        arr = new T[size];
+        vector<int> *v = new vector<int>(M);
+        if (v)
+        {
+            cout << "VECTOR OF TYPE " << type << " WITH M " << M << " CREATED\n";
+            delete (v);
+        }
+        else
+            cout << "MEMORY NOTE AVAILABLE\n";
     }
-};
+
+    else if (type.compare("long") == 0)
+    {
+        vector<long> *v = new vector<long>(M);
+        if (v)
+        {
+            cout << "VECTOR OF TYPE " << type << " WITH M " << M << " CREATED\n";
+            delete (v);
+        }
+        else
+            cout << "MEMORY NOTE AVAILABLE\n";
+    }
+    else if (type.compare("float") == 0)
+    {
+        vector<float> *v = new vector<float>(M);
+        if (v)
+        {
+            cout << "VECTOR OF TYPE " << type << " WITH M " << M << " CREATED\n";
+            delete (v);
+        }
+        else
+            cout << "MEMORY NOTE AVAILABLE\n";
+    }
+    else if (type.compare("double") == 0)
+    {
+        vector<double> *v = new vector<double>(M);
+        if (v)
+        {
+            cout << "VECTOR OF TYPE " << type << " WITH M " << M << " CREATED\n";
+            delete (v);
+        }
+        else
+            cout << "MEMORY NOTE AVAILABLE\n";
+    }
+    else if (type.compare("char") == 0)
+    {
+        vector<char> *v = new vector<char>(M);
+        if (v)
+        {
+            cout << "VECTOR OF TYPE " << type << " WITH M " << M << " CREATED\n";
+            delete (v);
+        }
+        else
+            cout << "MEMORY NOTE AVAILABLE\n";
+    }
+    else if (type.compare("string") == 0)
+    {
+        vector<string> *v = new vector<string>(M);
+        if (v)
+        {
+            cout << "VECTOR OF TYPE " << type << " WITH M " << M << " CREATED\n";
+            delete (v);
+        }
+        else
+            cout << "MEMORY NOTE AVAILABLE\n";
+    }
+    else
+    {
+        cout << "UNABLE TO IDENTITY THE DATA TYPE " << type << "\n";
+    }
+}
 int main()
 {
     char ch = 'y';
     while (ch == 'y')
     {
-        cout << "ENTER DATA TYPE AND SIZE\n";
+        cout << "ENTER DATA TYPE AND M\n";
         string type;
-        int size;
+        int M;
         while (true)
         {
-            if (cin >> type >> size && size > 0)
+            if (cin >> type >> M && M > 0)
             {
-                if (type.compare("int") == 0)
-                {
-                    Vector<int> *v = new Vector<int>(size);
-                    if (v)
-                    {
-                        cout << "VECTOR OF TYPE " << type << " WITH SIZE " << size << " CREATED\n";
-                        delete (v);
-                    }
-                    else
-                        cout << "MEMORY NOTE AVAILABLE\n";
-                    break;
-                }
-
-                else if (type.compare("long") == 0)
-                {
-                    Vector<long> *v = new Vector<long>(size);
-                    if (v)
-                    {
-                        cout << "VECTOR OF TYPE " << type << " WITH SIZE " << size << " CREATED\n";
-                        delete (v);
-                    }
-                    else
-                        cout << "MEMORY NOTE AVAILABLE\n";
-                    break;
-                }
-                else if (type.compare("float") == 0)
-                {
-                    Vector<float> *v = new Vector<float>(size);
-                    if (v)
-                    {
-                        cout << "VECTOR OF TYPE " << type << " WITH SIZE " << size << " CREATED\n";
-                        delete (v);
-                    }
-                    else
-                        cout << "MEMORY NOTE AVAILABLE\n";
-                    break;
-                }
-                else if (type.compare("double") == 0)
-                {
-                    Vector<double> *v = new Vector<double>(size);
-                    if (v)
-                    {
-                        cout << "VECTOR OF TYPE " << type << " WITH SIZE " << size << " CREATED\n";
-                        delete (v);
-                    }
-                    else
-                        cout << "MEMORY NOTE AVAILABLE\n";
-                    break;
-                }
-                else if (type.compare("char") == 0)
-                {
-                    Vector<char> *v = new Vector<char>(size);
-                    if (v)
-                    {
-                        cout << "VECTOR OF TYPE " << type << " WITH SIZE " << size << " CREATED\n";
-                        delete (v);
-                    }
-                    else
-                        cout << "MEMORY NOTE AVAILABLE\n";
-                    break;
-                }
-                else if (type.compare("string") == 0)
-                {
-                    Vector<string> *v = new Vector<string>(size);
-                    if (v)
-                    {
-                        cout << "VECTOR OF TYPE " << type << " WITH SIZE " << size << " CREATED\n";
-                        delete (v);
-                    }
-                    else
-                        cout << "MEMORY NOTE AVAILABLE\n";
-                    break;
-                }
-                else
-                {
-                    cout << "UNABLE TO IDENTITY THE DATA TYPE " << type << "\n";
-                    break;
-                }
+                create_vector(type, M);
+                break;
             }
             else
             {
